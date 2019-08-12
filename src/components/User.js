a2085adf7ebe78e1b386651d27c901642955121f
Vincent Kay <vincekay10@gmail.com>
2019-08-12 15:19:17 -0700

    Project 2

---
+++ src/components/User.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { createStore, applyMiddleware } from 'redux';
+
+
+const Types = {
+    CREATE_ITEM: "CREATE_ITEM",
+    DELETE_ITEM: "DELETE_ITEM"
+  };
+  // actions
+  const createItem = task => ({
+    type: Types.CREATE_ITEM,
+    payload: task
+  });
+  
+  const deleteItem = id => ({
+    type: Types.DELETE_ITEM,
+    payload: id
+  });
+  
+  export default {
+    createItem,
+    deleteItem,
+    Types
+  };
+
+  const Team = () => {
+      return (
+          <h1>this is the team page</h1>
+      )
+  }
